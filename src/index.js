5112fd5b19e6020fd11368d5c29adbc8b73ff19a
oksanaVlasenko <vlasenkoksenia642@gmail.com>
2020-05-04 17:22:24 +0300

    last commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BrowserRouter from "react-router-dom/BrowserRouter";
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
