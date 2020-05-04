8e22de307b8e931467f2426675423dd1869ed1f1
oksanaVlasenko <vlasenkoksenia642@gmail.com>
2020-05-04 18:06:20 +0300

    Updates

---
+++ precache-manifest.393f4d2e0c1860a74094afcecd799b83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2e04ac71da681ecd6ebcf7d14e3aa1",
+    "url": "/oksanaVlasenko.github.io/medical-analysis/index.html"
+  },
+  {
+    "revision": "4837f4c97e8a409077ef",
+    "url": "/oksanaVlasenko.github.io/medical-analysis/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "80cb8fb57b618d23dd43",
+    "url": "/oksanaVlasenko.github.io/medical-analysis/static/css/main.155763e1.chunk.css"
+  },
+  {
+    "revision": "4837f4c97e8a409077ef",
+    "url": "/oksanaVlasenko.github.io/medical-analysis/static/js/2.f2a863d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/oksanaVlasenko.github.io/medical-analysis/static/js/2.f2a863d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80cb8fb57b618d23dd43",
+    "url": "/oksanaVlasenko.github.io/medical-analysis/static/js/main.61886d5c.chunk.js"
+  },
+  {
+    "revision": "0fe5b3f0346b9e551ebf",
+    "url": "/oksanaVlasenko.github.io/medical-analysis/static/js/runtime-main.c8c3e215.js"
+  },
+  {
+    "revision": "2d61cbfe23adceb8802fbadf596ec39f",
+    "url": "/oksanaVlasenko.github.io/medical-analysis/static/media/color.2d61cbfe.PNG"
+  }
+]);
