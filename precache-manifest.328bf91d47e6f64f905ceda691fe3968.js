b137d6cce573fcafb6b30412809872f0ba45edac
oksanaVlasenko <vlasenkoksenia642@gmail.com>
2020-05-04 16:51:01 +0300

    Updates

---
+++ precache-manifest.328bf91d47e6f64f905ceda691fe3968.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98710b37111b138f1f3b93e71fd92196",
+    "url": "/medical-analysis/index.html"
+  },
+  {
+    "revision": "e004d7342f5d3e644aa0",
+    "url": "/medical-analysis/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "88d3ed58fb32eed2e243",
+    "url": "/medical-analysis/static/css/main.d4388371.chunk.css"
+  },
+  {
+    "revision": "e004d7342f5d3e644aa0",
+    "url": "/medical-analysis/static/js/2.07e9a467.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/medical-analysis/static/js/2.07e9a467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d3ed58fb32eed2e243",
+    "url": "/medical-analysis/static/js/main.8e3c7e34.chunk.js"
+  },
+  {
+    "revision": "b8ec9d7cc41dce7c5837",
+    "url": "/medical-analysis/static/js/runtime-main.9a4114ed.js"
+  },
+  {
+    "revision": "2d61cbfe23adceb8802fbadf596ec39f",
+    "url": "/medical-analysis/static/media/color.2d61cbfe.PNG"
+  }
+]);
